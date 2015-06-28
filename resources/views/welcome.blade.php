3712e23f7cbb4f5282333d261d413b599f3479b4
jomarjmm <jomarjmm@gmail.com>
2015-06-28 15:14:54 -0430

    First changes on Music

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <h1>Jomar Matos</h1>
             </div>
         </div>
     </body>
