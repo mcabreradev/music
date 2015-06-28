0332452d3df6aba9fdd484634a2c31289b7430fd
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2015-06-28 19:11:21 -0300

    resolucion de conflicto

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,43 @@
 <!-- resources/views/auth/login.blade.php -->
+@extends('index')
 
-<form method="POST" action="/auth/login">
-    {!! csrf_field() !!}
+@section('content')
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+<div class="row">
+  <div class="col-md-6 col-md-offset-2">
+    <div class="well bs-component">
+      <form class="form-horizontal" method="POST" action="/auth/login">
+        {!! csrf_field() !!}
+        <fieldset>
+          <legend>Login</legend>
+          <div class="form-group">
+            <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+            <div class="col-lg-10">
+              <input type="text" class="form-control" id="inputEmail" placeholder="Email" name="email" value="{{ old('email') }}">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="inputPassword" class="col-lg-2 control-label">Password</label>
+            <div class="col-lg-10">
+              <input type="password" class="form-control" placeholder="Password" name="password" id="password">
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Remember Me
+                </label>
+              </div>
+            </div>
+          </div>
 
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
+          <div class="form-group">
+            <div class="col-lg-10 col-lg-offset-2">
+              <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+          </div>
+        </fieldset>
+      </form>
+    <div id="source-button" class="btn btn-primary btn-xs" style="display: none;">&lt; &gt;</div></div>
+  </div>
 
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
-    </div>
+</div>
 
-    <div>
-        <button type="submit">Login</button>
-    </div>
-</form>
+@endsection
